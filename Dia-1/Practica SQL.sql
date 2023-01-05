50a046a35b82a49ce1ad28a3c86b123dc77dbab1
Yoel Aliaga <112524404+Yoel-Aliaga@users.noreply.github.com>
2023-01-04 21:23:18 -0500

    practica sql

---
+++ Dia-1/Practica SQL.sql
@@ -0,0 +1,120 @@
+-- Muestra la hora 
+select now();
+
+-- Modificamos la tabla para agregar la columna categoria_id
+-- ALTER TABLE productos ADD COLUMN categoria_id INT;
+-- Agregamos la relacion entre productos y categorias
+-- ALTER TABLE productos ADD FOREIGN KEY (categoria_id) REFERENCES categorias(id);
+
+-- ELIMINAR UNA BASE DE DATOS
+DROP DATABASE minimarket;
+
+-- ELIMINAR UNA TABLA 
+DROP TABLE categoria;
+
+-- 1.- Crear una base de datos llamada minimarket
+CREATE DATABASE IF NOT EXISTS minimarket;
+USE minimarket;
+
+-- 2.- Crear una tabla llamada categoria:
+-- id entero llave primaria y auto incrementable
+-- nombre text
+CREATE TABLE categorias(
+	id						INT 		PRIMARY KEY AUTO_INCREMENT,
+    nombre 					TEXT    
+);
+
+-- 3.- Crear una tabla llamada productos en la cual tengamos lo siguiente:
+-- id entero llave primaria y auto incrementable
+-- nombre text
+-- imagen text
+-- fecha_vencimiento date
+-- precio float
+-- disponible boolean defecto (verdadero)
+CREATE TABLE productos(
+	id						INT 		PRIMARY KEY AUTO_INCREMENT,
+    nombre 					TEXT,
+    imagen 					TEXT,
+    fecha_vencimiento		DATE,
+    precio 					FLOAT,
+    disponible 				BOOLEAN DEFAULT TRUE,     
+    -- Creas la llave foranea 
+    categoria_id			INT,
+    -- La llave foranea categoria_id se relaciona con el id de la tabla categorias  
+    FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+);
+
+-- 4. CREAR UNA TABLA LLAMADA ALMACEN en la cual seria de la sgte manera:
+-- id entero llave primaria y auto incrementable
+-- nombre text
+-- direccion text
+CREATE TABLE almacenes(
+	id						INT 		PRIMARY KEY AUTO_INCREMENT,
+    nombre 					TEXT,
+    direccion 				TEXT    
+);
+-- Una categoria puede tener varios productos pero un producto pertenece a una sola categoria
+
+
+-- Cambiar nombre de la tabla
+-- ALTER TABLE nombre_antiguo RENAME nombre_nuevo;
+
+
+-- Un producto puede estar en varios almacenes y a su vez cada almacen puede tener varios productos
+-- Relacion de muchos a muchos
+CREATE TABLE almacen_productos(
+	id				INT PRIMARY KEY AUTO_INCREMENT,
+    almacenes_id  	INT NOT NULL,
+    productos_id	INT NOT NULL,
+    FOREIGN KEY (almacenes_id) REFERENCES almacenes(id),
+    FOREIGN KEY (productos_id) REFERENCES productos(id)    
+);
+
+DROP TABLE almacen_producto;
+
+INSERT INTO categorias(id, nombre) VALUES (DEFAULT, 'Frutas'),
+										  (DEFAULT, 'Legumbres'),
+										  (DEFAULT, 'Snacks'),
+										  (DEFAULT, 'Otros');
+
+INSERT INTO productos (id, nombre, imagen, fecha_vencimiento, precio, disponible, categoria_id) VALUES (DEFAULT, 'Lechuga Carola', 'https://google.com','2022-01-10',2.90, true, 2),
+                                                                                                       (DEFAULT, 'Beterraga', 'https://google.com','2022-02-14',1.90, true, 2),
+                                                                                                       (DEFAULT, 'Papitas fritas', 'https://google.com','2025-01-10',2.90, true, 3),
+                                                                                                       (DEFAULT, 'Platano de seda', 'https://google.com','2022-01-10',4.75, true, 1),
+                                                                                                       (DEFAULT, 'Short playero', 'https://google.com','2022-12-31',39.90, true, 4),
+                                                                                                       (DEFAULT, 'Mandarina', 'https://google.com','2022-05-23',2.90, true, 1);
+
+INSERT INTO almacenes (id, nombre, direccion ) VALUES (DEFAULT, 'ALMACEN A', 'Calle los ruiseñores 1080'),
+                                                      (DEFAULT, 'ALMACEN B', 'Calle los girasoles 570'),
+                                                      (DEFAULT, 'ALMACEN C', 'Av Venezuela 3040');
+
+INSERT INTO almacen_producto (id, almacen_id, producto_id) VALUES (DEFAULT, 1, 1),
+                                                                  (DEFAULT, 1, 2),
+                                                                  (DEFAULT, 2, 2),
+                                                                  (DEFAULT, 2, 3),
+                                                                  (DEFAULT, 3, 4),
+                                                                  (DEFAULT, 1, 5),
+                                                                  (DEFAULT, 2, 6),
+                                                                  (DEFAULT, 3, 5);
+select * from productos;
+
+-- EJERCICIOS
+-- 1.- Mostrar todos los productos que tengan un precio mayor o igual que 3
+-- 2.- Mostrar todos los productos cuya fecha de vencimiento sea menor al 1 de agosto del 2022
+-- 3.- Mostrar los nombres de los productos concatenados con su URL de la imagen en un sola columna 
+-- que se llame 'producto imagen'
+
+SELECT * FROM productos WHERE precio >= 3;
+
+SELECT * FROM productos WHERE fecha_vencimiento < '2022-08-01';
+
+SELECT CONCAT(nombre, ' ' ,imagen ) AS 'producto imagen' FROM productos;
+
+
+
+
+
+
+
+
+
