925362b9cb7b623ca694986ada8a57d22daa8677
Yoel Aliaga <112524404+Yoel-Aliaga@users.noreply.github.com>
2023-01-06 21:14:58 -0500

    finalizacion

--- Dia-3/flask_con_db.py
+++ Dia-3/flask_con_db.py
@@ -33,17 +33,159 @@ def gestion_productos():
         cursor.execute("SELECT * FROM productos")
         productos = cursor.fetchall() # LIMIT INFINITO
         #cursor.fetchone() # LIMIT 1
-        print(productos)
-
+        # print(productos)
         # Cerrar la conexion
         cursor.close()
+
+        resultado= []
+        for producto in productos:
+            producto_dic={
+                'id': producto[0],
+                'nombre': producto[1],
+                'imagen': producto[2],
+                'fecha:vencimiento': producto[3].strftime('%Y-%m-%d'),#&H:&M:%S
+                'precio': producto[4],
+                'disponible': producto[5],
+                'categoria_id': producto[6],
+            }
+            resultado.append(producto_dic)
+            print(producto_dic)
+
         return{
-        'message' : 'Los productos son '
+        'message' : 'Los productos son ',
+        'content' : resultado
     }
     elif request.method == 'POST':
+        cursor = mysql.connection.cursor()
+        information = request.get_json()
+        # El %s covierter el contenido a un string,
+        # el %f convierte a flotante ,
+        # el % d covierte a entero
+        cursor.execute("INSERT INTO productos (id, nombre, imagen, fecha_vencimiento, precio, disponible, categoria_id)VALUES (DEFAULT,'%s','%s','%s',%f,%s,%d)" %(
+            information.get('nombre'),
+            information.get('imagen'),
+            information.get('fecha_vencimiento'),
+            information.get('precio'),
+            information.get('disponible'),
+            information.get('categoria_id'),
+        ))
+        mysql.connection.commit()
+        cursor.close()
+
         return {
-        'message' : 'PRoducto creado exitosamente'
+        'message' : 'Producto creado exitosamente'
     }
+
+
+def validar_producto(id):
+    # Creamos la conexion 
+        conexion = mysql.connection.cursor()
+        # Ejecutamos el select con la clausula del id
+        conexion.execute("SELECT * FROM productos WHERE id= %d" % (id))
+        # Esto es lo mismo pero agregar la f para dar formato
+        # conexion.execute(f"SELECT * FROM productos WHERE id = {id}")
+        resultado = conexion.fetchone()
+        conexion.close()
+        print(resultado)
+        return resultado
+
+
+
+@app.route("/producto/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
+def gestion_un_producto(id):
+    if request.method == 'GET':
+        resultado = validar_producto(id)
+        if resultado is None:
+            return{
+                'message':'Producto no existe'
+            }
+        else:
+            producto={
+                    'id':resultado[0],
+                    'nombre':resultado[1],
+                    'imagen':resultado[2],
+                    'fecha_vencimiento':resultado[3].strftime('%Y-%m-%d'),
+                    'precio':resultado[4],
+                    'disponible':resultado[5],
+                    'categoria_id':resultado[6]
+            }
+
+            # conexion.close()
+            # print(resultado)        
+
+            return {
+                'content':producto                                
+            }
+    elif request.method == 'PUT':
+        # Primero validamos que4 esista el producto
+        # conexion = mysql.connection.cursor()
+        # conexion.execute(f"SELECT * FROM productos WHERE id = {id}") 
+        # resultado = conexion.fetchone()
+        resultado = validar_producto(id)
+        if resultado is None:
+            return{
+                'message': 'Producto no existe'
+            }
+        else:
+            data=request.get_json()
+
+            conexion =mysql.connection.cursor()
+            conexion.execute("UPDATE productos SET nombre= %s, precio=%s, fecha_vencimiento=%s, categoria_id=%s, disponible=%s, imagen=%s WHERE id = %s" , [
+                data.get('nombre'),
+                data.get('precio'),
+                data.get('fecha_vencimiento'),
+                data.get('categoria_id'),
+                data.get('disponible'),
+                data.get('imagen'),
+                resultado[0] # id del producto
+            ])
+            mysql.connection.commit()
+            conexion.close()
+
+            return {
+                'message': 'Producto actualizado exitosamente'                
+            }
+    elif request.method == 'DELETE':
+        # DELETE FROM productos WHERE id = .....
+        # no se necesita nada del boy 
+        # hacer la validacion si el producto no existe 
+        resultado = validar_producto(id)
+        if resultado is None:
+            return{
+                'message':'El producto no existe'
+            }
+        else:
+            # data=request.get_json()
+            conexion =mysql.connection.cursor()
+            # primero eliminar ese producto en los almacenes 
+            conexion.execute("DELETE FROM productos WHERE id = %s AND delete_rule='CASCADE' " , [id])
+            
+            mysql.connection.commit()
+            conexion.close()
+
+            return {
+                'message': 'Producto ELIMINADO exitosamente'                
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
